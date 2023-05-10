a2efd929d8fb1f16a856db7997b7ee6ae65d8cd2
Anurag <anusrivas07@gmail.com>
2023-05-10 21:50:00 +0530

    'Database project'

--- lib/loginFrom.dart
+++ lib/loginFrom.dart
@@ -13,6 +13,7 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm>{
 
   //SharedPreferences sharedPreferences= await SharedPreferences.getInstance();
+
   final email_con=TextEditingController();
   final pass_con=TextEditingController();
   var   dbHelper;
@@ -45,9 +46,7 @@ class _LoginFormState extends State<LoginForm>{
     String email=email_con.text;
     String pass=pass_con.text;
     await dbHelper.getLoginPage(email,pass).then((userdata) async {
-
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Successfully Registered")));
-
       /*if(userdata!=null){*/
       /*   SharedPreferences prefs = await SharedPreferences.getInstance();
         prefs.setString("e", email);*/
@@ -58,78 +57,51 @@ class _LoginFormState extends State<LoginForm>{
       }*/
     });
   }
-  @override
-  Widget build(BuildContext context) {
+
+  Widget feilds({required String name, required var controller, required IconData icon}){
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            filled: true,
+            hintStyle: TextStyle(color: Colors.grey[800]),
+            labelText: name,
+            icon: Icon(icon,color: Colors.blueAccent),
+            fillColor: Colors.white
+        ),
+      ),
+    );
+   }
+    @override
+    Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.cyan,
       appBar: AppBar(
         backgroundColor: Colors.greenAccent,
         title: Text('Login form'),
       ),
-       body: Container(
-              child: Column(
-              children: [
-                Container(
-                margin: EdgeInsets.all(20),
-                child: TextField(
-                  controller: email_con,
-                  decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        filled: true,
-                        hintStyle: TextStyle(color: Colors.grey[800]),
-                        labelText: 'email',
-                        icon: Icon(Icons.format_list_numbered),
-                        iconColor: Colors.blueAccent,
-                        fillColor: Colors.white
-                    ),
-                  ),
-                ),
-            Container(
-              margin: EdgeInsets.all(20),
-              child: TextField(
-              controller: pass_con,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                filled: true,
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                fillColor: Colors.white,
-                labelText: 'password',
-                icon: Icon(Icons.drive_file_rename_outline_outlined),
-                iconColor: Colors.blueAccent,
-
-              ),
-              obscureText: true,
-            ),
-          ),
+       body: Column(
+        children: [
+           feilds(name: 'E_mail', controller: email_con, icon: (Icons.format_list_numbered)),
+           feilds(name: 'Password', controller: pass_con, icon: (Icons.password)),
           SizedBox(height:5,),
-          ElevatedButton(
-              onPressed: (){
-                sigin;
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Successfully Registered")));
-              },
-               child: Text('Sign in',style: TextStyle(fontWeight: FontWeight.w200,fontSize:20,),),
+          ElevatedButton( onPressed: (){
+            sigin;
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Successfully Registered")));
+            },
+            child: Text('Sign in',style: TextStyle(fontWeight: FontWeight.w200,fontSize:20,),),
           ),
           ElevatedButton(
-              onPressed: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                       return Registration_Form();
-                      }
-                    )
-                );
-              },
-              child:
-                Text('Register For New User',style: TextStyle(fontWeight: FontWeight.w200,fontSize: 20,),)
-          ),
-        ],
-       ),
-    ),
-  );
+           onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) =>  Registration_Form())),
+            child: Text('Register For New User',style: TextStyle(fontWeight: FontWeight.w200,fontSize: 20,),)
+           ),
+         ],
+        ),
+   );
 
   }
 }
