b1101fa691900101b694e69ce35b245690ccf4be
Anurag <anusrivas07@gmail.com>
2023-05-12 11:29:37 +0530

    'Database project'

--- lib/screens/Firstpage.dart
+++ lib/screens/Firstpage.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:loginandregistration/screens/personalities.dart';
 
-class firstPage extends StatefulWidget {
+class firstPage extends StatelessWidget {
 
-  @override
-  State<firstPage> createState() => _firstPageState();
-}
-
-class _firstPageState extends State<firstPage> {
-
-  List<String> namelist = ['Prime Minister\'s', 'Doctor\'s', 'Actor\'s'];
+  List<String>name = ['Prime Ministers', 'Doctors', 'Actors'];
 
   @override
   Widget build(BuildContext context) {
-    var namelist;
     return Scaffold(
       appBar: AppBar(
+        leading: Icon(Icons.arrow_back,color: Colors.white,),
         title: Text('Categories of Personality'),
         centerTitle: true,
         backgroundColor: Color(0xff3a3e3e),
       ),
       body: ListView.builder(
-          itemCount: namelist.length,
+          itemCount: name.length,
           itemBuilder: (BuildContext context, int pos){
           return GestureDetector(
             onTap: (){
                 Navigator.push(
                   context, MaterialPageRoute(builder: (context){
-                    return namelist(p: pos);
+                    return Personalities(p: pos);
                    }
                   ),
                 );
             },
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(namelist[pos]),
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage('images/flag.jpg'),fit: BoxFit.cover),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    ),
+                  ),
+                  // Text(name[pos]),
               ],
             ),
           );
--- lib/screens/personalities.dart
+++ lib/screens/personalities.dart
@@ -7,18 +7,18 @@ class Personalities extends StatelessWidget {
   Personalities({required  this.p});
 
   List<Modelclass> prime = [
-    Modelclass(name: 'Narender Modi', imagee: '../images/modi.jpg'),
-    Modelclass(name: 'Atal Bihari Vajypee', imagee: '../images/atal.jpg'),
+    Modelclass(name: 'Narender Modi', imagee: 'images/modi.jpg'),
+    Modelclass(name: 'Atal Bihari Vajypee', imagee: 'images/atal.jpg'),
   ];
 
   List<Modelclass> doc = [
-    Modelclass(name: 'Harsh Vardhan', imagee: '../images/vardhan.jpg'),
-    Modelclass(name: 'Mansukh Mandviyua', imagee: '../images/mansukh.jpg'),
+    Modelclass(name: 'Harsh Vardhan', imagee: 'images/vardhan.jpg'),
+    Modelclass(name: 'Mansukh Mandviyua', imagee: 'images/mansukh.jpg'),
   ];
 
   List<Modelclass> actor = [
-    Modelclass(name: 'Salman Khan', imagee: '../images/salman.jpg'),
-    Modelclass(name: 'Akshay Kumar', imagee: '../images/akshay.jpg'),
+    Modelclass(name: 'Salman Khan', imagee: 'images/salman.jpg'),
+    Modelclass(name: 'Akshay Kumar', imagee: 'images/akshay.jpg'),
   ];
 
   @override
